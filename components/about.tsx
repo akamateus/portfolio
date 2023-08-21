51ccf668d17400c4134243c49261739481b5286b
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-21 15:31:47 +0200

    added about component

---
+++ components/about.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section>
+      <h2>About me </h2>
+      <p>
+        I am a passionate Junior Web Developer who has recently completed an
+        intensive full-stack bootcamp, immersing myself in the world of web
+        technologies including HTML, CSS, JavaScript, TypeScript, React,
+        Next.js, and MongoDB, among others. My technical skills are complemented
+        by strong communication abilities, stemming from my background in
+        Journalism. I've honed my expertise in digital marketing and social
+        media, which adds a unique layer to my skill set. Drawing from my
+        knowledge in web design, I seamlessly integrate aesthetic principles
+        into my frontend work, creating user-centric and visually appealing
+        digital experiences.
+      </p>
+      My insatiable curiosity defines my approach to life and work. I am driven
+      by an unquenchable thirst for knowledge, always seeking to broaden my
+      horizons and embrace new challenges. This enthusiasm keeps me in a
+      constant state of motion, whether I'm deepening my understanding of
+      emerging technologies or uncovering novel ways to enhance my coding
+      prowess. With an unwavering commitment to growth, I find joy in the
+      process of continuous learning, a quality that propels me forward as I
+      strive to create innovative and impactful solutions in the ever-evolving
+      landscape of web development.
+      <p></p>
+    </section>
+  );
+}
