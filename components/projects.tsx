392df6171522f5cfde3185ccc296e1bc46a8c130
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-22 13:42:09 +0200

    styled even

--- components/projects.tsx
+++ components/projects.tsx
@@ -22,8 +22,8 @@ type ProjectProps = (typeof projectsData)[0];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
-    <section className="group bg-gray-300 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
+    <section className="group bg-gray-300 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8">
+      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
         <h3 className="text-2xl font-semibold"> {title}</h3>
         <p className="mt-2 leading-relaxed text-grey-700">{description}</p>
         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
