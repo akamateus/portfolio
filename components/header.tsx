78e15ba0910752bf6fa25345f8ff39706930bcde
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-24 16:36:37 +0200

    installed clsx for tailwind

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
 
 export default function Header() {
+  const [activeSection, setActiveSection] = useState("Home");
   return (
     <header className="z-[999] relative">
       <motion.div
