6cef1559f07f4b6ba68d39ac0abf4b0c9db397d6
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-25 11:53:59 +0200

    set active-section-context, need to fix createContext Error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -22,8 +23,10 @@ export default function RootLayout({
         <div className="bg-[#fff7c5] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#f5bcbc] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2x1:left-[-5rem]"></div>
 
-        <Header />
-        {children}
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import { links } from "@/lib/data";
 
 type SectionName = (typeof links)[number]["name"];
 
-export default function ActiveSectionContextProvider() {
+type ActiveSectionContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+};
+
+const ActiveSectionContext = createContext<ActiveSectionContextType | null>(
+  null
+);
+export default function ActiveSectionContextProvider({
+  children,
+}: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
-  return <div>ActiveSectionContextProvider</div>;
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
+  );
 }
