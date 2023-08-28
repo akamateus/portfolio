18b303c4a6ba6390d608459c13ffd2104545d85e
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-28 15:10:15 +0200

    added npm i --save-dev @types/react-vertical-timeline-component

---
+++ components/experience.tsx
@@ -0,0 +1,15 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import React from "react";
+import SectionHeading from "./section-heading";
+
+export default function Experience() {
+  return (
+    <section id="experience">
+      <SectionHeading>My experience</SectionHeading>
+    </section>
+  );
+}
