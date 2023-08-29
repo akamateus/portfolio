bfafd6cb2f66d6df5e953d9297a9c8aac24e7ddf
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-29 17:02:04 +0200

    changed scroll action for experience section and took of bottom height for whole page

--- components/experience.tsx
+++ components/experience.tsx
@@ -10,7 +10,7 @@ import { experiencesData } from "@/lib/data";
 
 export default function Experience() {
   return (
-    <section id="experience">
+    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
