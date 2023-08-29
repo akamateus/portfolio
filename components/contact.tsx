ea2dcdb0e6726d2991d9d9f87e9bc50db1e230ad
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-29 18:25:27 +0200

    put skills and experience in viwe

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,5 +1,56 @@
+"use client";
+
 import React from "react";
+import SectionHeading from "./section-heading";
+import { FaPaperPlane } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
 
 export default function Contact() {
-  return <section id="contact"></section>;
+  const { ref } = useSectionInView("Contact");
+  return (
+    <motion.section
+      ref={ref}
+      id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{ opacity: 1 }}
+      transition={{
+        duration: 1,
+      }}
+    >
+      <SectionHeading>Contact me</SectionHeading>
+      <p className="text-gray-700 -mt-6">
+        Contact me directly at{" "}
+        <a
+          className="text-amber-500 underline hover:cursor-pointer"
+          href="mailto:mateusoliveirac@hotmail.com"
+        >
+          mateusoliveirac@hotmail.com
+        </a>{" "}
+        or through this form bellow:
+      </p>
+
+      <form className="mt-10 flex flex-col ">
+        <input
+          className="h-14 px-4 rounded-lg borderBlack"
+          type="email"
+          placeholder=" Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          placeholder="Your message 🫰🏽"
+        />
+        <button
+          type="submit"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-amber-500 hover:bg-amber-400 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105"
+        >
+          Send
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+        </button>
+      </form>
+    </motion.section>
+  );
 }
--- components/experience.tsx
+++ components/experience.tsx
@@ -7,10 +7,12 @@ import "react-vertical-timeline-component/style.min.css";
 import React from "react";
 import SectionHeading from "./section-heading";
 import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
+  const { ref } = useSectionInView("Experience");
   return (
-    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
+    <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
