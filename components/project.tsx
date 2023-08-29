915fb903b5cbe42296bb94bda295d593d11316f7
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-29 13:25:07 +0200

    put a link on projects

--- components/project.tsx
+++ components/project.tsx
@@ -12,6 +12,7 @@ export default function Project({
   description,
   tags,
   imageUrl,
+  link,
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
 
@@ -33,26 +34,27 @@ export default function Project({
       className=" group mb-3 sm:mb-8 last:mb-0 "
     >
       <section className=" bg-gray-100 max-w-[42rem] border-2 rounded-lg border-black/10 overflow-hidden sm:pr-8 relative sm:h-[20rem]  hover:bg-amber-100 transition group-even:pl-8">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold"> {title}</h3>
-          <p className="mt-2 leading-relaxed text-grey-700">{description}</p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-amber-500 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <a href={link}>
+          <div className="cursor-pointer pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+            <h3 className="text-2xl font-semibold"> {title}</h3>
+            <p className="mt-2 leading-relaxed text-grey-700">{description}</p>
+            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+              {tags.map((tag, index) => (
+                <li
+                  className="bg-amber-500 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+                  key={index}
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <Image
-          src={imageUrl}
-          alt="My projects"
-          quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          <Image
+            src={imageUrl}
+            alt="My projects"
+            quality={95}
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
           transition 
           group-hover:scale-[1.04]
           group-hover:-translate-x-3
@@ -64,7 +66,8 @@ export default function Project({
           group-even:group-hover:rotate-2
   
           group-even:right-[initial] group-even:-left-40"
-        />
+          />
+        </a>
       </section>
     </motion.div>
   );
