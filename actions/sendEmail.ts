848445879a9ca07eb2e43453e0226ad387bb5b4d
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-31 14:55:25 +0200

    latest

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,7 +1,18 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmail = async (formData: FormData) => {
   console.log("RUNNING ON SERVER");
   console.log(formData.get("senderEmail"));
   console.log(formData.get("message"));
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "mateusoliveirac@hotmail.com",
+    subject: "message from contact form",
+    text: "Hello world",
+  });
 };
