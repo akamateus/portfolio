b1798c27c1559466634a45edcad4e7a6674c004e
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-04 14:34:35 +0200

    added style

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer>
-      <small>&copy; 2023 AkaMateus. All rights reserved.</small>
-      <p>
-        <span>About this website:</span> build with React & Next.js (App Router
-        & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email
-        & Resend, Vercel.
+    <footer className="mb-10 px-4 text-center text-amber-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2023 AkaMateus. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> build with
+        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email & Resend, Vercel.
       </p>
     </footer>
   );
