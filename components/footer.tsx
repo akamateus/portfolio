449d0c9b1bf67aa759482cb008405c04131ba8af
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-09-04 14:30:03 +0200

    added footer to layout

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2023 AkaMateus. All rights reserved.</small>
+      <p>
+        <span>About this website:</span> build with React & Next.js (App Router
+        & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email
+        & Resend, Vercel.
+      </p>
+    </footer>
+  );
+}
